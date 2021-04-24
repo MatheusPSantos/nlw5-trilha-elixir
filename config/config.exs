df2d0af5d3d568997f7c1200b71e52a43d9c70ec
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-04-23 21:59:43 -0300

    remove inamana e place inmana

--- config/config.exs
+++ config/config.exs
@@ -10,7 +10,7 @@ use Mix.Config
 config :inmana,
   ecto_repos: [Inmana.Repo]
 
-config :inmana, Inamana.Repo,
+config :inmana, Inmana.Repo,
   migration_primary_key: [type: :binary_id],
   migration_foreign_key: [type: :binary_id]
 
