7e16a9cb02c58307ee96169b939cfa1a1001df8d
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-04-24 23:33:16 -0300

    criando o método de inserir supply e criando delegate

--- lib/inmana.ex
+++ lib/inmana.ex
@@ -6,7 +6,10 @@ defmodule Inmana do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
-  alias Inmana.Restaurants.Create
+  alias Inmana.Restaurants.Create, as: RestaurantCreate
+  alias Inmana.Supplies.Create, as: SupplyCreate
 
-  defdelegate create_restaurant(params), to: Create, as: :call
+  defdelegate create_restaurant(params), to: RestaurantCreate, as: :call
+
+  defdelegate create_supply(params), to: SupplyCreate, as: :call
 end
