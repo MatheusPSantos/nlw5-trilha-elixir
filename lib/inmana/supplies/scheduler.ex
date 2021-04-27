86f8303d5de51ae609c6ce960c220a5198f8161d
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-04-26 22:46:26 -0300

    criando função que faz o cron, e a cliente

--- lib/inmana/supplies/scheduler.ex
+++ lib/inmana/supplies/scheduler.ex
@@ -1,17 +1,28 @@
 defmodule Inmana.Supplies.Scheduler do
   use GenServer
 
+  alias Inmana.Supplies.ExpirationNotification
+
+  def start_link(_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  @impl true
   def init(state \\ %{}) do
+    schedule_notification()
     {:ok, state}
   end
 
-  # async
-  def handle_cast({:put, key, value}, state) do
-    {:noreply, Map.put(state, key, value)}
+  @impl true
+  def handle_info(:generate, state) do
+    ExpirationNotification.send()
+
+    schedule_notification()
+
+    {:noreply, state}
   end
 
-  # sync
-  def handle_call({:get, key}, _from, state) do
-    {:reply, Map.get(state, key), state}
+  defp schedule_notification do
+    Process.send_after(self(), :generate, 1000 * 10)
   end
 end
