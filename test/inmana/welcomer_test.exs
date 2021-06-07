568feaefc465b24dda9da0eb665991173a7fb478
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-06-07 19:35:57 -0300

    bussiness logic tests

--- test/inmana/welcomer_test.exs
+++ test/inmana/welcomer_test.exs
@@ -6,8 +6,28 @@ defmodule Inmana.WelcomerTest do
   describe "welcome/1" do
     test "when the user is special, returns special message" do
       params = %{"name" => "banana", "age" => "42"}
-      result = Welcomer.welcome(params)
       expected_result = {:ok, "You are very special"}
+
+      result = Welcomer.welcome(params)
+
+      assert result == expected_result
+    end
+
+    test "when the user is not special, returns a message" do
+      params = %{"name" => "Jhon", "age" => "23"}
+      expected_result = {:ok, "Welcome Jhon"}
+
+      result = Welcomer.welcome(params)
+
+      assert result == expected_result
+    end
+
+    test "when the user is under age, returns an error" do
+      params = %{"name" => "Jhon", "age" => "12"}
+      expected_result = {:ok, "You shall not pass Jhon"}
+
+      result = Welcomer.welcome(params)
+
       assert result == expected_result
     end
   end
